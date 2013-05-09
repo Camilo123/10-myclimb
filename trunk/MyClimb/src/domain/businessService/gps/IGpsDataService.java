6fefb56899303aa5ebfd8e237a32363cd17513bc
szpssky@gmail.com <szpssky@gmail.com@43dfa174-4ba7-a760-154d-ba9c1915aae6>
2013-05-09 07:30:38 +0000

    完成domain/service/gps 接口声明

--- trunk/MyClimb/src/domain/businessService/gps/IGpsDataService.java
+++ trunk/MyClimb/src/domain/businessService/gps/IGpsDataService.java
@@ -3,7 +3,20 @@ package domain.businessService.gps;
 import domain.businessEntity.gps.ClimbData;
 
 public interface IGpsDataService {
+	//添加登山记录
+	public boolean addClimbData(ClimbData data);
+	
+	//删除登山记录
+	public boolean deleteCilmbDataByID(int climbId);
+	
+	//删除全部记录
+	public boolean deleteAll();
 	
 	//获取登山记录
 	public ClimbData getClimbData();
+	
+	//根据climbID获取登山记录
+	public ClimbData getClimbDataById(int climbId);
+	
+	
 }
