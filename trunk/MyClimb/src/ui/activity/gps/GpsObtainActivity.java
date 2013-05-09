f18706d7d4fbc52def0a1152ec73b49b7179ff9e
yczheng861291633@gmail.com <yczheng861291633@gmail.com@43dfa174-4ba7-a760-154d-ba9c1915aae6>
2013-05-09 09:06:27 +0000

    禁止横屏，修改startAndStop按钮效果

--- trunk/MyClimb/src/ui/activity/gps/GpsObtainActivity.java
+++ trunk/MyClimb/src/ui/activity/gps/GpsObtainActivity.java
@@ -2,19 +2,18 @@ package ui.activity.gps;
 
 import java.util.List;
 
+import ui.activity.ActivityOfAF4Ad;
+import ui.viewModel.ModelErrorInfo;
+import ui.viewModel.ViewModel;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.mysport.ui.R;
-
-
-import ui.activity.ActivityOfAF4Ad;
-import ui.viewModel.ModelErrorInfo;
-import ui.viewModel.ViewModel;
 /**
  * @author DreamTeam 沈志鹏
  */
@@ -26,6 +25,7 @@ public class GpsObtainActivity extends ActivityOfAF4Ad {
 	private TextView tv_latitude;
 	private TextView tv_time;
 	private ImageView iv_startAndStop;
+	private Button b_startAndStop;
 	private ImageView iv_record;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -44,16 +44,16 @@ public class GpsObtainActivity extends ActivityOfAF4Ad {
 	protected void initControlsAndRegEvent() {
 		//获取相应控件id
 		iv_startAndStop = (ImageView) findViewById(R.id.iv_startAndStop);
+		b_startAndStop = (Button)findViewById(R.id.b_startAndStop);
 		tv_altitude = (TextView) findViewById(R.id.tv_altitude);
 		tv_direction = (TextView) findViewById(R.id.tv_direction);
 		tv_speed = (TextView) findViewById(R.id.tv_speed);
 		tv_longitude = (TextView) findViewById(R.id.tv_longitude);
 		tv_latitude = (TextView) findViewById(R.id.tv_latitude);
 		tv_time = (TextView) findViewById(R.id.tv_time);
-		iv_startAndStop = (ImageView) findViewById(R.id.iv_startAndStop);
 		iv_record = (ImageView) findViewById(R.id.iv_record);
 		
-		iv_startAndStop.setOnClickListener(new View.OnClickListener() {
+		b_startAndStop.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View v) {
 				Intent intent =new Intent(GpsObtainActivity.this,RecDetailsActivity.class);
