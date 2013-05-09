c9c17d22ccc68d4c711fac12dd326c3f266f1cb8
szpssky@gmail.com <szpssky@gmail.com@43dfa174-4ba7-a760-154d-ba9c1915aae6>
2013-05-09 07:30:38 +0000

    完成domain/service/gps 接口声明

---
+++ MyClimb/src/domain/businessService/gps/GpsDataService.java
@@ -0,0 +1,37 @@
+package domain.businessService.gps;
+
+import domain.businessEntity.gps.ClimbData;
+
+public class GpsDataService implements IGpsDataService {
+
+	@Override
+	public boolean addClimbData(ClimbData data) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean deleteCilmbDataByID(int climbId) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean deleteAll() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public ClimbData getClimbData() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ClimbData getClimbDataById(int climbId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
