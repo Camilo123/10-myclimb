1dd2d6adc6da5cbfba7d978f303bbbc6bb0b03d6
yczheng861291633@gmail.com <yczheng861291633@gmail.com@43dfa174-4ba7-a760-154d-ba9c1915aae6>
2013-05-16 05:29:26 +0000

    以RadioGroup 来实现tab菜单，增加按键效果

---
+++ MyClimb/src/ui/activity/map/FourActivity.java
@@ -0,0 +1,16 @@
+package ui.activity.map;
+
+import android.app.Activity;
+import android.os.Bundle;
+import com.mysport.ui.*;
+
+public class FourActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		this.setContentView(R.layout.activity_record);//这个java 文件是临时的 所以这里面的东西都是暂时这样设置
+	}
+
+}
