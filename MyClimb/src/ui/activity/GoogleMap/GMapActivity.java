2f24cd1062c6f1a57a973c7eb5ffef349b01b5d2
yczheng861291633@gmail.com <yczheng861291633@gmail.com@43dfa174-4ba7-a760-154d-ba9c1915aae6>
2013-05-28 15:54:50 +0000

    基本实现轨迹记录（貌似精度不是很高 还需优化）

--- MyClimb/src/ui/activity/GoogleMap/GMapActivity.java
+++ MyClimb/src/ui/activity/GoogleMap/GMapActivity.java
@@ -1,8 +1,17 @@
 
 package ui.activity.GoogleMap;
 
+/**
+ * @author DreamTeam 郑运春
+ */
+
+
 import static com.google.android.gms.maps.GoogleMap.MAP_TYPE_SATELLITE;
-import android.content.Intent;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
 import android.graphics.Color;
 import android.location.Location;
 import android.os.Bundle;
@@ -12,7 +21,9 @@ import android.widget.Toast;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GooglePlayServicesClient.ConnectionCallbacks;
 import com.google.android.gms.common.GooglePlayServicesClient.OnConnectionFailedListener;
+import com.google.android.gms.location.LocationClient;
 import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
@@ -26,37 +37,34 @@ import com.mysport.ui.R;
 public class GMapActivity extends FragmentActivity 
 implements ConnectionCallbacks, OnConnectionFailedListener, LocationListener{
  
-	
-    private static final LatLng MELBOURNE = new LatLng(-37.81319, 144.96298);
-    private static final LatLng SYDNEY = new LatLng(-33.87365, 151.20689);
-    private static final LatLng ADELAIDE = new LatLng(-34.92873, 138.59995);
-    private static final LatLng PERTH = new LatLng(-31.95285, 115.85734);
+	//代码还需优化 改动  所以略显乱
 
-    private static final LatLng LHR = new LatLng(51.471547, -0.460052);
-    private static final LatLng LAX = new LatLng(33.936524, -118.377686);
-    private static final LatLng JFK = new LatLng(40.641051, -73.777485);
-    private static final LatLng AKL = new LatLng(-37.006254, 174.783018);
 
-    private static final int WIDTH_MAX = 50;
-    private static final int HUE_MAX = 360;
-    private static final int ALPHA_MAX = 255;
     private Polyline mMutablePolyline;
     //经纬度
     private double latitude;
     private double longitude;
     private String Name;
+    private LocationClient mLocationClient;
     
-	
+    private List<LatLng> points = new ArrayList<LatLng>();
+    
+    
     private GoogleMap mMap;
     private UiSettings mUiSettings;
 
   
+    private static final LocationRequest REQUEST = LocationRequest.create()
+        .setInterval(5000)         // 5 seconds
+        .setFastestInterval(16)    // 16ms = 60fps
+        .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+    
     
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_googlemap);
-    
+ 
         SupportMapFragment mapFragment =
                 (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
         
@@ -70,7 +78,25 @@ implements ConnectionCallbacks, OnConnectionFailedListener, LocationListener{
         
         setUpMapIfNeeded();
         
-        
+//      points.add(new LatLng(20,120));
+//      points.add(new LatLng(25,125));
+//      points.add(new LatLng(30,130));
+//      points.add(new LatLng(35,135));
+
+    //  drawRouteOnMap();
+    }
+    
+//    class draw implements Runnable{
+//
+//		@Override
+//		public void run() {
+//			// TODO Auto-generated method stub
+//			drawRouteOnMap();
+//		
+//		}
+//    }
+    
+    public void setMarker(){
         //获取由RecDetailsActivity传过来的位置信息
 		Bundle bundle = getIntent().getExtras();
 		if(bundle != null)
@@ -83,7 +109,6 @@ implements ConnectionCallbacks, OnConnectionFailedListener, LocationListener{
 	       	 
 	       mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(longitude,latitude),15));   	
 		}
-		
     }
     
     
@@ -109,8 +134,120 @@ implements ConnectionCallbacks, OnConnectionFailedListener, LocationListener{
 //            
             
             }
+        setUpLocationClientIfNeeded();
+        mLocationClient.connect();
     }
+    
+    @Override
+    public void onPause() {
+      super.onPause();
+      if (mLocationClient != null) {
+        mLocationClient.disconnect();
+      }
+    }
+    
+    private void setUpLocationClientIfNeeded() {
+        if (mLocationClient == null) {
+          mLocationClient = new LocationClient(
+              getApplicationContext(),
+              this,  // ConnectionCallbacks
+              this); // OnConnectionFailedListener
+        }
+      }
 
+
+    
+    private void addLatLngPoint(double longitude,double latitude){
+    	double lat = latitude;
+    	double lon = longitude;
+		
+    	points.add(new LatLng(lon,lat));
+    	
+    }
+    
+ /*   //想在这里取得定位信息
+    
+//    private void myLocation(){
+//    	
+//    	LocationManager locManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+//
+//		Location location = locManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+//	
+//		
+//		latitude = location.getLatitude();
+//		longitude = location.getLongitude();
+//		
+//		addLatLngPoint(latitude,longitude);	
+//		
+//	 
+//			LocationListener locationListener = new LocationListener() {
+//				
+//				//当坐标改变时触发此函数，如果Provider传进相同的坐标，它就不会被触发 
+//				@Override
+//				public void onLocationChanged(Location location) {
+//					if (location != null) {   
+//						
+//						latitude = location.getLatitude();
+//						longitude = location.getLongitude();  
+//					}
+//					
+//					addLatLngPoint(latitude,longitude);
+//				}
+//			};
+//			locManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,1000, 0,(android.location.LocationListener) locationListener);   
+//			if(location != null){   
+//				latitude = location.getLatitude(); //经度   
+//				longitude = location.getLongitude(); //纬度
+//				
+//				addLatLngPoint(latitude,longitude);
+//			}   
+//		}
+//    	
+//    
+////    locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+////    currentLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+////    if( currentLocation == null){
+////    currentLocation = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+////    }
+////    
+//    
+//    
+////    	Toast.makeText(getApplicationContext(), "222",
+////			     Toast.LENGTH_SHORT).show();
+////    	
+////    	LocationManager locationManager =(LocationManager)getSystemService(Context.LOCATION_SERVICE);
+////    	if(locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)){
+////    		Location location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+////    		if(location !=null){
+////    			latitude = location.getLatitude();
+////    			longitude = location.getLongitude();
+////    			
+////    			addLatLngPoint(latitude,longitude);
+////    			
+////    			String s1 = Double.toString(latitude)+Double.toString(longitude);
+////    			Toast.makeText(getApplicationContext(), "默认1 + s1",
+////    				     Toast.LENGTH_SHORT).show();
+////    		}
+////    	}else{
+////
+////    	locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 3000, 8,new LocationListener(){
+////
+////			@Override
+////			public void onLocationChanged(Location location) {
+////				// TODO Auto-generated method stub
+////				latitude = location.getLatitude();
+////    			longitude = location.getLongitude();
+////				addLatLngPoint(latitude,longitude);
+////				String s2 = Double.toString(latitude)+Double.toString(longitude);
+////    			Toast.makeText(getApplicationContext(), "默认2 + s2",
+////    				     Toast.LENGTH_SHORT).show();
+////			}
+////			
+////    		};
+////    	}
+//  
+  
+    */
     private void setUi(){
         mUiSettings.setZoomControlsEnabled(true);
         mUiSettings.setCompassEnabled(true);
@@ -136,57 +273,83 @@ implements ConnectionCallbacks, OnConnectionFailedListener, LocationListener{
     private void setUpMap() {
         mMap.setMyLocationEnabled(true);
         mUiSettings = mMap.getUiSettings();
-    	
-        // Circle centered at Sydney.  This polyline will be mutable.
-        PolylineOptions options = new PolylineOptions();
-        int radius = 5;
-        int numPoints = 100;
-        double phase = 2 * Math.PI / numPoints;
-        for (int i = 0; i <= numPoints; i++) {
-            options.add(new LatLng(SYDNEY.latitude + radius * Math.sin(i * phase),
-                    SYDNEY.longitude + radius * Math.cos(i * phase)));
-        }
-     
-        
-        options.add(new LatLng(26,119 ));
-        
-        mMutablePolyline = mMap.addPolyline(options
-                .color(Color.RED)
-                .width(5));
     }
 
-
-
-
+    private void drawRouteOnMap(){
+        if(points.size() >= 2)
+        {
+		    	PolylineOptions options = new PolylineOptions();
+		//        int radius = 5;
+		//        int numPoints = 100;
+		//        double phase = 2 * Math.PI / numPoints;
+		//        for (int i = 0; i <= numPoints; i++) {
+		//            options.add(new LatLng(SYDNEY.latitude + radius * Math.sin(i * phase),
+		//                    SYDNEY.longitude + radius * Math.cos(i * phase)));
+		//        }
+		     
+		        
+		//        options.add(new LatLng(26,119 ));
+		        
+		    for(int i=0;i<points.size();i++)
+		    {
+		    	options.add(points.get(i));
+				String s2 = Double.toString(points.get(i).longitude)+"    "+Double.toString(points.get(i).latitude);
+				Toast.makeText(getApplicationContext(), "坐标 "+ i+":  " + s2,
+		 		     Toast.LENGTH_SHORT).show();
+		    	
+		    }
+		    	
+		    	
+		      //  options.addAll(points);
+		        
+		        mMutablePolyline = mMap.addPolyline(options
+		                .color(Color.RED)
+		                .width(3));
+        }
+    }
 
 	@Override
-	public void onLocationChanged(Location arg0) {
+	public void onLocationChanged(Location location) {
 		// TODO Auto-generated method stub
+		latitude = location.getLatitude();
+		longitude = location.getLongitude();
+		
+		BigDecimal   b1   =   new   BigDecimal(latitude);  
+		BigDecimal   b2   =   new   BigDecimal(longitude);
+		
+		double   f1   =   b1.setScale(4,   BigDecimal.ROUND_HALF_UP).doubleValue();  
+		double   f2   =   b2.setScale(4,   BigDecimal.ROUND_HALF_UP).doubleValue(); 
+		
+		addLatLngPoint(f1,f2);
+		
+//		String s2 = Double.toString(longitude)+"    "+Double.toString(latitude);
+//		Toast.makeText(getApplicationContext(), "坐标：    " + s2,
+// 		     Toast.LENGTH_SHORT).show();
+		
+		drawRouteOnMap();
+		
+//		String s1 = Integer.toString(points.size());
+//		Toast.makeText(getApplicationContext(), "长度:" + s1,
+//	 		     Toast.LENGTH_SHORT).show();
+		
 		
 	}
-
-
-
-
 
 	@Override
 	public void onConnectionFailed(ConnectionResult arg0) {
 		// TODO Auto-generated method stub
-		
 	}
+	
+	  @Override
+	  public void onConnected(Bundle connectionHint) {
+	    mLocationClient.requestLocationUpdates(
+	        REQUEST,
+	        this);  // LocationListener
+	  }
 
-
-	@Override
-	public void onConnected(Bundle arg0) {
-		// TODO Auto-generated method stub
-		
-	}
-
-
-
+ 
 	@Override
 	public void onDisconnected() {
 		// TODO Auto-generated method stub
-		
 	}
 }
